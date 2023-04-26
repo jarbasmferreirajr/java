322a60bc1e7a1394c035bd9c2ce0eba1741e28b2
Jarbas de M. Ferreira Jr <72043035+jarbasmferreirajr@users.noreply.github.com>
2023-04-26 15:28:16 -0300

    Exercicios de Java do curso Novos Caminhos
    Exemplo de uma operação de "soma de valores" em Java.

---
+++ Linguagem de Programação Java (Exercicios do curso Novos Caminhos)/Encontro1/Exemplo003/src/Exemplo003.java
@@ -0,0 +1,16 @@
+
+public class Exemplo003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int valor1 = 100;
+		int valor2 = 150;
+		
+		int soma = valor1 + valor2;
+		
+		System.out.println(soma);
+
+	}
+
+}
